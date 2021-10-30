1f06b1cfe040c4574d548dd9b9dffcf00af1f756
xeonqq <xeonqq@gmail.com>
2021-10-30 16:27:56 +0200

    Correct formular

--- second_order_dynamic.py
+++ second_order_dynamic.py
@@ -10,19 +10,21 @@ den = [T ** 2, T * 2, 1]
 
 system = lti(num, den)
 d_system = system.to_discrete(dt)
-s, x_d = dstep(d_system)
+methods=["zoh","euler"]
+ts = np.arange(0, 10, dt)
+ax = plt.subplot()
+for method in methods:
+    d_system= system.to_discrete(dt,method)
+    s, x_d = dstep(d_system)
+    ax.step(s, np.squeeze(x_d), label="discrete response ({})".format(method))
+    ax.legend()
+
 # print(d_system)
 # print(s, x_d)
-ts = np.arange(0, 10, dt)
 
 
 response_t, response = system.step(X0=0, T=ts)
-
-
-ax = plt.subplot()
 ax.plot(response_t, response, label="laplace response")
-ax.legend()
-ax.step(s, np.squeeze(x_d), label="discrete response")
 ax.legend()
 
 
@@ -34,21 +36,23 @@ class System(object):
         self._y_t_minus_1 = 0
         self._v = 0
         self._v_t_minus_1 = 0
+        self._u_t_minus_1 = 0
 
     def forward_euler(self, u):
 
         self._y_t = self._y_t_minus_1 + self._v_t_minus_1 * self._dt
-        self._v_t = self._v_t_minus_1 + (u - self._y_t_minus_1 - 2 * self._T * self._v_t_minus_1) / T ** 2 * self._dt
+        self._v_t = self._v_t_minus_1 + (self._u_t_minus_1 - self._y_t_minus_1 - 2 * self._T * self._v_t_minus_1) / T ** 2 * self._dt
 
         self._y_t_minus_1 = self._y_t
         self._v_t_minus_1 = self._v_t
+        self._u_t_minus_1 = u
         return self._y_t
 
 
 u = 1
-results_iterative_forward_euler = [0]
+results_iterative_forward_euler = []
 system = System(T, dt)
-for i in ts[1:]:
+for i in ts:
     results_iterative_forward_euler.append(system.forward_euler(u))
 
 
